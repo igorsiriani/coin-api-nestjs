3282aa1e99ccab98cb8f59743e8f17b9fa90a26c
Igor Siriani <igorsiriani@gmail.com>
2024-07-15 02:51:01 -0300

    Aded Docker

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const cors ={
-    origin: ['http://localhost:3000'],
-    methods: 'GET,PUT,POST,DELETE'
+    origin: "*",
+    methods: "GET,PUT,POST,DELETE"
   }
 
-  app.enableCors(cors)
+  app.enableCors(cors);
   await app.listen(3000);
 }
 bootstrap();
